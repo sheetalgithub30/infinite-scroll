cbb1289698c1027767f5aa2146b9da88ad08f3ff
sheetal <sheetalkumarilife@gmail.com>
2024-06-16 12:27:35 +0530

    added infinite scroll

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,44 @@ import './App.css'
 function App() {
   const [page, setPage] = useState(1);
   const[data,setData] = useState([]);
+
+  async function getData(){
+    const response = await fetch(`https://api.unsplash.com/photos/?page=${page}&per_page=20&client_id=XCtYKfKmCVr18RlEzs7cx1LFRcuhFbs7fAWcvkMHwQA`);
+    const result = await response.json();
+    setData((prev)=>[...prev,...result]);
+  }
+
+  function infiniteScroll(){
+    try{
+      if(window.innerHeight+ document.documentElement.scrollTop +1 >= document.documentElement.scrollHeight){
+        setPage((prev)=>prev+1);
+        // getData();
+      }
+    }
+    catch(error){
+      console.log(error);
+    }
+  }
+
   useEffect(()=>{
-     async function getData(){
-       const response = await fetch(`https://api.unsplash.com/photos/?page=${page}&per_page=40&client_id=XCtYKfKmCVr18RlEzs7cx1LFRcuhFbs7fAWcvkMHwQA`);
-       const result = await response.json();
-       setData(result);
-     }
      getData();
-  },[])
+  },[page])
+
+
+  useEffect(()=>{
+    window.addEventListener("scroll",infiniteScroll);
+    return () =>window.removeEventListener("scroll",infiniteScroll)
+  },[]);
 
   return (
     <div id="main">
       {
       data.map((e)=>{
          return (
-          <div id="images">
-            <img src={e.urls.thumb}></img>
+          <div id="images"> 
+            <p id="name">{e.user.name}</p>
+            <p id="created">{e.created_at}</p>
+            <img src={e.urls.small}></img>
           </div>
          ) 
       })
